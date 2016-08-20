6bce234e0ecb68d9dc5327c8d0873100fab0da3a
kekosh <kenta0540@gmail.com>
2016-08-20 09:07:20 +0000

    have want method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
 
   # 他のユーザーをフォローする
@@ -31,20 +35,44 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    haves = haves.find_by(item_id: item.id)
+    haves.destroy if haves
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    wants = wants.find_by(item_id: item.id)
+    wants.destroy if wants
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
+
+  # # 他のユーザーをフォローする
+  # def follow(other_user)
+  #   following_relationships.find_or_create_by(followed_id: other_user.id)
+  # end
+
+  # # フォローしているユーザーをアンフォローする
+  # def unfollow(other_user)
+  #   following_relationship = following_relationships.find_by(followed_id: other_user.id)
+  #   following_relationship.destroy if following_relationship
+  # end
+
+  # # あるユーザーをフォローしているかどうか？
+  # def following?(other_user)
+  #   following_users.include?(other_user)
+  # end
